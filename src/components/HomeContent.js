a473851a932276ae0e78e5dc0f7a7877d9d3b13e
Joxongir Baxtiyarovich <jahongir.r1999@gmail.com>
2022-07-02 18:32:11 +0500

    media

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -5,14 +5,11 @@ import { PROJECT_ROUTE } from "../utils/const";
 const HomeContent = () => {
   return (
     <div className="home_container" id="home">
-      <div className="bg">
-        <img src="" alt="" />
-      </div>
+      <div className="bg"></div>
       <div className="home_content">
         <h1>
           <p className="home_collapse_1">Road</p> for
           <p className="home_collapse_2">Junior</p> <br />
-          коллекция
         </h1>
         <Link to={PROJECT_ROUTE} className="show_me">
           <span>Смотреть</span>
